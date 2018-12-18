d6594749c146d7b7c03dea533410192da09dc267
caimengyun <13570929941@163.com>
2018-12-18 23:03:01 +0800

    Complete display of weather

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by caimengyun on 18-12-17.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
